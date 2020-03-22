89196bc5d648b9e299a250a0d0edfab48ee704a2
smr <1246054720@qq.com>
2020-03-22 15:37:32 +0800

    fix bug

--- src/assets/request/http.js
+++ src/assets/request/http.js
@@ -30,57 +30,57 @@ instance.interceptors.request.use(
 
 // 响应拦截器
 instance.interceptors.response.use(
-		// 请求成功
-		//res => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
-		res => {
-			if (res.status === 200) {
-				return Promise.resolve(res);
-			} else {
-				return Promise.reject(res);
-			}
-		},
-		// 请求失败
-		error => {
-			const {
-				response
-			} = error;
+	// 请求成功
+	//res => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
+	res => {
+		if (res.status === 200) {
+			return Promise.resolve(res);
+		} else {
+			return Promise.reject(res);
+		}
+	},
+	// 请求失败
+	error => {
+		const {
+			response
+		} = error;
+		if (response) {
 			if (response) {
-				if (response) {
-					switch (response.status) {
-						// 400: 客户端请求的语法错误，服务器无法理解
-						case 400:
-							console.log("400客户端请求的语法错误，服务器无法理解");
-							break;
-						case 401:
-							Message({
-								message: "登录过期,即将跳转登录",
-								type: "error"
-							});
-							break;
-							// 404请求不存在
-						case 404:
-							Message({
-								message: "请求的资源不存在",
-								type: "error"
-							});
-							break;
-						case 408:
-							Message({
-								message: "网络延时，请稍后",
-								type: "error"
-							});
-							break;
-						default:
-							Message("网络异常");
-					}
-					return Promise.reject(response);
-				} else {
-					// 处理断网的情况
-					// eg:请求超时或断网时，更新state的network状态
-					// network状态在app.vue中控制着一个全局的断网提示组件的显示隐藏
-					// 关于断网组件中的刷新重新获取数据，会在断网组件中说明
+				switch (response.status) {
+					// 400: 客户端请求的语法错误，服务器无法理解
+					case 400:
+						console.log("400客户端请求的语法错误，服务器无法理解");
+						break;
+					case 401:
+						Message({
+							message: "登录过期,即将跳转登录",
+							type: "error"
+						});
+						break;
+						// 404请求不存在
+					case 404:
+						Message({
+							message: "请求的资源不存在",
+							type: "error"
+						});
+						break;
+					case 408:
+						Message({
+							message: "网络延时，请稍后",
+							type: "error"
+						});
+						break;
+					default:
+						Message("网络异常");
 				}
+				return Promise.reject(response);
+			} else {
+				// 处理断网的情况
+				// eg:请求超时或断网时，更新state的network状态
+				// network状态在app.vue中控制着一个全局的断网提示组件的显示隐藏
+				// 关于断网组件中的刷新重新获取数据，会在断网组件中说明
 			}
-		);
-
-		export default instance;
+		}
+	}
+);
+export default instance;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -10,14 +12,14 @@ Vue.prototype.$axios = Axios;
 Vue.config.productionTip = false;
 
 router.beforeEach((to, from, next) => {
-  /* 路由发生变化修改页面title */
-  if (to.meta.title) {
-    document.title = to.meta.title;
-  }
-  next();
+	/* 路由发生变化修改页面title */
+	if (to.meta.title) {
+		document.title = to.meta.title;
+	}
+	next();
 });
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount("#app");
